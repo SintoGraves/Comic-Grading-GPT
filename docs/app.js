e6aeb3c0aa6def0e9299b4ad2856d912ee5998d9
SintoGraves <sintograves@gmail.com>
2025-12-08 21:05:18 -0500

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -333,8 +333,7 @@ const STAMP_RULES = {
 };
 
 
-// === Lookup: issues that DO contain a Marvel Value Stamp ===
-// (unchanged – your big table)
+/// === Lookup: issues that DO contain a Marvel Value Stamp ===
 const VALUE_STAMP_INDEX = {
   // Adventure Into Fear
   "adventure into fear#21": true,
@@ -375,7 +374,7 @@ const VALUE_STAMP_INDEX = {
   "amazing spider-man#156": true,
   "amazing spider-man#157": true,
 
-  // ... (rest of your existing VALUE_STAMP_INDEX; unchanged)
+  // Astonishing Tales
   "astonishing tales#23": true,
   "astonishing tales#24": true,
   "astonishing tales#25": true,
@@ -384,10 +383,466 @@ const VALUE_STAMP_INDEX = {
   "astonishing tales#28": true,
   "astonishing tales#33": true,
 
-  // (keep the rest of the table exactly as you already have it)
+  // Avengers
   "avengers#121": true,
   "avengers#122": true,
-  // etc...
+  "avengers#123": true,
+  "avengers#124": true,
+  "avengers#125": true,
+  "avengers#126": true,
+  "avengers#127": true,
+  "avengers#128": true,
+  "avengers#129": true,
+  "avengers#130": true,
+  "avengers#131": true,
+  "avengers#132": true,
+  "avengers#143": true,
+  "avengers#144": true,
+  "avengers#147": true,
+
+  // Giant-Size Avengers
+  "giant-size avengers#3": true,
+
+  // Captain America
+  "captain america#171": true,
+  "captain america#172": true,
+  "captain america#173": true,
+  "captain america#174": true,
+  "captain america#175": true,
+  "captain america#176": true,
+  "captain america#177": true,
+  "captain america#178": true,
+  "captain america#179": true,
+  "captain america#180": true,
+  "captain america#181": true,
+  "captain america#182": true,
+  "captain america#184": true,
+  "captain america#192": true,
+  "captain america#193": true,
+  "captain america#194": true,
+  "captain america#195": true,
+  "captain america#197": true,
+  "captain america#198": true,
+
+  // Captain Marvel
+  "captain marvel#32": true,
+  "captain marvel#33": true,
+  "captain marvel#34": true,
+  "captain marvel#35": true,
+  "captain marvel#36": true,
+  "captain marvel#41": true,
+  "captain marvel#42": true,
+  "captain marvel#43": true,
+
+  // Champions
+  "champions#3": true,
+  "champions#4": true,
+  "champions#5": true,
+
+  // Conan
+  "conan#36": true,
+  "conan#37": true,
+  "conan#38": true,
+  "conan#39": true,
+  "conan#40": true,
+  "conan#41": true,
+  "conan#42": true,
+  "conan#43": true,
+  "conan#44": true,
+  "conan#49": true,
+  "conan#59": true,
+  "conan#60": true,
+  "conan#62": true,
+
+  // Creatures on the Loose
+  "creatures on the loose#28": true,
+  "creatures on the loose#29": true,
+  "creatures on the loose#30": true,
+  "creatures on the loose#31": true,
+  "creatures on the loose#32": true,
+  "creatures on the loose#33": true,
+  "creatures on the loose#36": true,
+
+  // Daredevil
+  "daredevil#108": true,
+  "daredevil#109": true,
+  "daredevil#110": true,
+  "daredevil#111": true,
+  "daredevil#112": true,
+  "daredevil#113": true,
+  "daredevil#114": true,
+  "daredevil#115": true,
+  "daredevil#116": true,
+  "daredevil#117": true,
+  "daredevil#118": true,
+  "daredevil#120": true,
+  "daredevil#127": true,
+  "daredevil#129": true,
+  "daredevil#130": true,
+  "daredevil#131": true,
+  "daredevil#132": true,
+  "daredevil#134": true,
+  "daredevil#136": true,
+
+  // Defenders
+  "defenders#13": true,
+  "defenders#15": true,
+  "defenders#16": true,
+  "defenders#17": true,
+  "defenders#19": true,
+  "defenders#20": true,
+  "defenders#23": true,
+  "defenders#31": true,
+  "defenders#32": true,
+  "defenders#33": true,
+  "defenders#35": true,
+  "defenders#37": true,
+  "defenders#38": true,
+
+  // Giant-Size Defenders
+  "giant-size defenders#3": true,
+
+  // Doctor Strange (v2)
+  "doctor strange#1": true,
+  "doctor strange#2": true,
+  "doctor strange#3": true,
+  "doctor strange#4": true,
+  "doctor strange#5": true,
+  "doctor strange#6": true,
+  "doctor strange#12": true,
+  "doctor strange#13": true,
+
+  // Fantastic Four
+  "fantastic four#144": true,
+  "fantastic four#145": true,
+  "fantastic four#146": true,
+  "fantastic four#147": true,
+  "fantastic four#149": true,
+  "fantastic four#150": true,
+  "fantastic four#151": true,
+  "fantastic four#152": true,
+  "fantastic four#153": true,
+  "fantastic four#154": true,
+  "fantastic four#155": true,
+  "fantastic four#159": true,
+  "fantastic four#160": true,
+  "fantastic four#166": true,
+  "fantastic four#167": true,
+  "fantastic four#168": true,
+  "fantastic four#170": true,
+
+  // Giant-Size Fantastic Four
+  "giant-size fantastic four#4": true,
+
+  // Frankenstein
+  "frankenstein#9": true,
+  "frankenstein#10": true,
+  "frankenstein#11": true,
+  "frankenstein#12": true,
+  "frankenstein#13": true,
+  "frankenstein#14": true,
+  "frankenstein#16": true,
+
+  // Ghost Rider
+  "ghost rider#5": true,
+  "ghost rider#6": true,
+  "ghost rider#7": true,
+  "ghost rider#8": true,
+  "ghost rider#9": true,
+  "ghost rider#11": true,
+  "ghost rider#16": true,
+  "ghost rider#18": true,
+
+  // Giant-Size Creatures / Dracula
+  "giant-size creatures#1": true,
+  "giant-size dracula#5": true,
+
+  // Incredible Hulk
+  "hulk#174": true,
+  "hulk#175": true,
+  "hulk#176": true,
+  "hulk#177": true,
+  "hulk#179": true,
+  "hulk#180": true,
+  "hulk#181": true,
+  "hulk#182": true,
+  "hulk#183": true,
+  "hulk#184": true,
+  "hulk#186": true,
+  "hulk#195": true,
+  "hulk#196": true,
+  "hulk#197": true,
+  "hulk#198": true,
+  "hulk#199": true,
+  "hulk#200": true,
+
+  // Inhumans
+  "inhumans#3": true,
+  "inhumans#6": true,
+
+  // Invaders
+  "invaders#1": true,
+  "invaders#3": true,
+  "invaders#4": true,
+  "invaders#7": true,
+
+  // Iron Fist
+  "iron fist#1": true,
+  "iron fist#3": true,
+  "iron fist#4": true,
+
+  // Iron Man
+  "iron man#67": true,
+  "iron man#68": true,
+  "iron man#69": true,
+  "iron man#70": true,
+  "iron man#71": true,
+  "iron man#81": true,
+  "iron man#82": true,
+  "iron man#83": true,
+  "iron man#84": true,
+  "iron man#86": true,
+  "iron man#87": true,
+  "iron man#88": true,
+
+  // Jungle Action
+  "jungle action#9": true,
+  "jungle action#10": true,
+  "jungle action#11": true,
+  "jungle action#12": true,
+  "jungle action#13": true,
+  "jungle action#20": true,
+  "jungle action#21": true,
+
+  // Ka-Zar
+  "ka-zar#2": true,
+  "ka-zar#3": true,
+  "ka-zar#4": true,
+  "ka-zar#5": true,
+  "ka-zar#6": true,
+  "ka-zar#8": true,
+  "ka-zar#10": true,
+  "ka-zar#12": true,
+  "ka-zar#15": true,
+  "ka-zar#16": true,
+  "ka-zar#17": true,
+
+  // Kull
+  "kull#13": true,
+  "kull#14": true,
+  "kull#15": true,
+
+  // Man-Thing
+  "man-thing#3": true,
+  "man-thing#4": true,
+  "man-thing#5": true,
+  "man-thing#6": true,
+  "man-thing#7": true,
+  "man-thing#8": true,
+  "man-thing#9": true,
+  "man-thing#10": true,
+  "man-thing#11": true,
+  "man-thing#13": true,
+  "man-thing#14": true,
+
+  // Giant-Size Man-Thing
+  "giant-size man-thing#3": true,
+  "giant-size man-thing#4": true,
+  "giant-size man-thing#5": true,
+
+  // Marvel Feature
+  "marvel feature#3": true,
+
+  // Marvel Premiere
+  "marvel premiere#15": true,
+  "marvel premiere#16": true,
+  "marvel premiere#17": true,
+  "marvel premiere#18": true,
+  "marvel premiere#19": true,
+  "marvel premiere#20": true,
+  "marvel premiere#23": true,
+  "marvel premiere#24": true,
+  "marvel premiere#25": true,
+  "marvel premiere#29": true,
+  "marvel premiere#30": true,
+
+  // Marvel Spotlight
+  "marvel spotlight#14": true,
+  "marvel spotlight#15": true,
+  "marvel spotlight#16": true,
+  "marvel spotlight#17": true,
+  "marvel spotlight#18": true,
+  "marvel spotlight#19": true,
+  "marvel spotlight#20": true,
+  "marvel spotlight#26": true,
+  "marvel spotlight#27": true,
+
+  // Marvel Team-Up
+  "marvel team-up#19": true,
+  "marvel team-up#20": true,
+  "marvel team-up#21": true,
+  "marvel team-up#22": true,
+  "marvel team-up#23": true,
+  "marvel team-up#24": true,
+  "marvel team-up#25": true,
+  "marvel team-up#26": true,
+  "marvel team-up#27": true,
+  "marvel team-up#28": true,
+  "marvel team-up#29": true,
+  "marvel team-up#30": true,
+  "marvel team-up#33": true,
+  "marvel team-up#39": true,
+  "marvel team-up#41": true,
+  "marvel team-up#42": true,
+  "marvel team-up#43": true,
+  "marvel team-up#44": true,
+  "marvel team-up#45": true,
+
+  // Marvel Two-In-One
+  "marvel two-in-one#2": true,
+  "marvel two-in-one#3": true,
+  "marvel two-in-one#4": true,
+  "marvel two-in-one#5": true,
+  "marvel two-in-one#6": true,
+  "marvel two-in-one#7": true,
+  "marvel two-in-one#12": true,
+  "marvel two-in-one#13": true,
+  "marvel two-in-one#14": true,
+  "marvel two-in-one#15": true,
+  "marvel two-in-one#18": true,
+
+  // Master of Kung Fu
+  "master of kung fu#17": true,
+  "master of kung fu#18": true,
+  "master of kung fu#19": true,
+  "master of kung fu#21": true,
+  "master of kung fu#22": true,
+  "master of kung fu#23": true,
+  "master of kung fu#24": true,
+  "master of kung fu#25": true,
+  "master of kung fu#31": true,
+  "master of kung fu#38": true,
+  "master of kung fu#39": true,
+  "giant-size master of kung fu#3": true,
+
+  // Omega
+  "omega#1": true,
+  "omega#2": true,
+  "omega#3": true,
+
+  // Power Man
+  "power man#18": true,
+  "power man#19": true,
+  "power man#20": true,
+  "power man#21": true,
+  "power man#23": true,
+  "power man#24": true,
+  "power man#29": true,
+  "power man#32": true,
+  "power man#33": true,
+
+  // Sgt. Fury
+  "sgt. fury#118": true,
+  "sgt. fury#119": true,
+  "sgt. fury#120": true,
+
+  // Skull the Slayer
+  "skull the slayer#3": true,
+  "skull the slayer#4": true,
+
+  // Son of Satan
+  "son of satan#1": true,
+  "son of satan#5": true,
+
+  // Strange Tales
+  "strange tales#173": true,
+  "strange tales#174": true,
+  "strange tales#175": true,
+  "strange tales#176": true,
+
+  // Sub-Mariner
+  "sub-mariner#69": true,
+  "sub-mariner#70": true,
+  "sub-mariner#71": true,
+  "sub-mariner#72": true,
+
+  // Supernatural Thrillers
+  "supernatural thrillers#8": true,
+  "supernatural thrillers#9": true,
+  "supernatural thrillers#11": true,
+  "supernatural thrillers#12": true,
+
+  // Super-Villain Team-Up
+  "super-villain team-up#4": true,
+  "super-villain team-up#5": true,
+
+  // Thor
+  "thor#221": true,
+  "thor#222": true,
+  "thor#223": true,
+  "thor#224": true,
+  "thor#225": true,
+  "thor#226": true,
+  "thor#227": true,
+  "thor#228": true,
+  "thor#229": true,
+  "thor#231": true,
+  "thor#232": true,
+  "thor#235": true,
+  "thor#237": true,
+  "thor#238": true,
+  "thor#243": true,
+  "thor#244": true,
+  "thor#245": true,
+  "thor#246": true,
+  "thor#248": true,
+  "thor#249": true,
+
+  // Tomb of Dracula
+  "tomb of dracula#18": true,
+  "tomb of dracula#19": true,
+  "tomb of dracula#20": true,
+  "tomb of dracula#21": true,
+  "tomb of dracula#22": true,
+  "tomb of dracula#23": true,
+  "tomb of dracula#24": true,
+  "tomb of dracula#25": true,
+  "tomb of dracula#26": true,
+  "tomb of dracula#27": true,
+  "tomb of dracula#28": true,
+  "tomb of dracula#29": true,
+  "tomb of dracula#30": true,
+  "tomb of dracula#31": true,
+  "tomb of dracula#34": true,
+  "tomb of dracula#35": true,
+  "tomb of dracula#37": true,
+  "tomb of dracula#39": true,
+  "tomb of dracula#40": true,
+  "tomb of dracula#41": true,
+  "tomb of dracula#42": true,
+  "tomb of dracula#43": true,
+  "tomb of dracula#44": true,
+  "tomb of dracula#45": true,
+
+  // War Is Hell
+  "war is hell#11": true,
+  "war is hell#13": true,
+
+  // Werewolf by Night
+  "werewolf by night#15": true,
+  "werewolf by night#16": true,
+  "werewolf by night#17": true,
+  "werewolf by night#19": true,
+  "werewolf by night#20": true,
+  "werewolf by night#21": true,
+  "werewolf by night#22": true,
+  "werewolf by night#23": true,
+  "werewolf by night#24": true,
+  "werewolf by night#25": true,
+  "werewolf by night#26": true,
+  "werewolf by night#27": true,
+
+  // Worlds Unknown
   "worlds unknown#6": true,
   "worlds unknown#7": true,
   "worlds unknown#8": true
@@ -783,52 +1238,93 @@ if (stampApplies) {
     const interiorSysSec   = computeSection(baseScore, interiorSysDeduction, interiorSysMax);
     const interiorSysGrade = pickGrade(GRADES, interiorSysSec.score);
 
-    // === Overall / true grade ===
-    const totalDeduction = (
-      spineDeduction +
-      structAttachDeduction +
-      stapleRustDeduction +
-      frontCoverDeduction + backCoverDeduction +
-      frontSurfaceDeduction + backSurfaceDeduction +
-      frontCornerDeduction + backCornerDeduction +
-      frontGlossDeduction + backGlossDeduction +
-      frontUVDeduction + backUVDeduction +
-      frontColorDeduction + backColorDeduction +
-      frontWaterDeduction + backWaterDeduction +
-      coverMarksDeduction +
-      pageToneDeduction + interiorTearDeduction + interiorStainDeduction +
-      stampDeduction
-    );
+   // === Overall / true grade (Balanced, Option B) ===
 
-    const overallRaw = baseScore - totalDeduction;
+// Keep totalDeduction so GM override still works:
+const totalDeduction = (
+  spineDeduction +
+  structAttachDeduction +
+  stapleRustDeduction +
+  frontCoverDeduction + backCoverDeduction +
+  frontSurfaceDeduction + backSurfaceDeduction +
+  frontCornerDeduction + backCornerDeduction +
+  frontGlossDeduction + backGlossDeduction +
+  frontUVDeduction + backUVDeduction +
+  frontColorDeduction + backColorDeduction +
+  frontWaterDeduction + backWaterDeduction +
+  coverMarksDeduction +
+  pageToneDeduction + interiorTearDeduction + interiorStainDeduction +
+  stampDeduction
+);
 
-    const overallMax = Math.min(
-      spineRule.max_score,
-      structAttachRule.max_score,
-      stapleRustRule.max_score,
-      frontCoverRule.max_score,
-      backCoverRule.max_score,
-      frontSurfaceRule.max_score,
-      backSurfaceRule.max_score,
-      frontCornerRule.max_score,
-      backCornerRule.max_score,
-      frontGlossRule.max_score,
-      backGlossRule.max_score,
-      frontUVRule.max_score,
-      backUVRule.max_score,
-      frontColorRule.max_score,
-      backColorRule.max_score,
-      frontWaterRule.max_score,
-      backWaterRule.max_score,
-      coverMarksRule.max_score,
-      pageToneRule.max_score,
-      interiorTearRule.max_score,
-      interiorStainRule.max_score,
-      stampRule.max_score
-    );
+// 1. Structural scores: these set the **ceiling**
+const structuralScores = [
+  spineSec.score,
+  structAttachSec.score,
+  stapleRustSec.score,
+  interiorSysSec.score
+];
+const minStructuralScore = Math.min(...structuralScores);
 
-    let overallScore = Math.min(overallRaw, overallMax);
-    let overallGrade = pickGrade(GRADES, overallScore);
+// Map lowest structural score to a "Balanced" ceiling
+function getStructuralCeiling(score) {
+  if (score >= 9.0) return 10.0;  // very strong structure
+  if (score >= 7.0) return 8.0;   // solid but not perfect
+  if (score >= 5.0) return 6.0;   // mid-grade structure
+  if (score >= 3.0) return 4.0;   // low-grade structure
+  if (score >= 1.5) return 2.0;   // very weak structure
+  return 1.5;                     // worst cases 0.5–1.0
+}
+
+const structuralCeiling = getStructuralCeiling(minStructuralScore);
+
+// 2. Group scores (0–10) using averages
+const spineGroupScore =
+  (spineSec.score + structAttachSec.score + stapleRustSec.score) / 3;
+
+const coverGroupScore =
+  (frontCoverSec.score + backCoverSec.score +
+   frontSurfaceSec.score + backSurfaceSec.score +
+   frontCornerSec.score + backCornerSec.score) / 6;
+
+const finishGroupScore =
+  (frontColorSysSec.score + backColorSysSec.score +
+   frontWaterSec.score + backWaterSec.score +
+   coverMarksSec.score) / 5;
+
+// Interior is already combined as a system
+const interiorGroupScore = interiorSysSec.score;
+
+// 3. Balanced weighted score (still 0–10)
+const weightedScore =
+  spineGroupScore   * 0.35 +
+  coverGroupScore   * 0.35 +
+  finishGroupScore  * 0.15 +
+  interiorGroupScore* 0.15;
+
+// 4. Apply the structural ceiling
+let overallScore = Math.min(weightedScore, structuralCeiling);
+let overallGrade = pickGrade(GRADES, overallScore);
+
+// 5. Build “why capped” note (if ceiling actually limited it)
+let capNote = "";
+if (weightedScore > structuralCeiling + 0.05) {
+  const limitingParts = [];
+  if (spineSec.score === minStructuralScore) {
+    limitingParts.push(`Spine / Edge (${spineGrade.short})`);
+  }
+  if (structAttachSec.score === minStructuralScore) {
+    limitingParts.push(`Cover / Centerfold Attachment (${structAttachGrade.short})`);
+  }
+  if (stapleRustSec.score === minStructuralScore) {
+    limitingParts.push(`Staple Condition (${stapleRustGrade.short})`);
+  }
+  if (interiorSysSec.score === minStructuralScore) {
+    limitingParts.push(`Interior (overall) (${interiorSysGrade.short})`);
+  }
+
+  capNote = `Overall grade capped at ${overallGrade.short} due to structural limits from: ${limitingParts.join(", ")}.`;
+}
 
     // === Presentation grade (front view only) ===
     const frontPresentationDeduction = (
@@ -865,16 +1361,17 @@ if (stampApplies) {
     let presentationGrade = pickGrade(GRADES, presentationScore);
 
     // === If GM candidate and absolutely no deductions, bump to 10.0 ===
-    let gmNote = "";
-    if (gmCandidate && totalDeduction === 0) {
-      overallScore = 10.0;
-      overallGrade = pickGrade(GRADES, overallScore);
+let gmNote = "";
+if (gmCandidate && totalDeduction === 0) {
+  overallScore = 10.0;
+  overallGrade = pickGrade(GRADES, overallScore);
 
-      presentationScore = 10.0;
-      presentationGrade = pickGrade(GRADES, presentationScore);
+  presentationScore = 10.0;
+  presentationGrade = pickGrade(GRADES, presentationScore);
 
-      gmNote = "Gem Mint override applied: all selected conditions are top-tier with no deductions.";
-    }
+  gmNote = "Gem Mint override applied: all selected conditions are top-tier with no deductions.";
+  capNote = "";  // no cap when everything is perfect
+}
 
     // === Visible wear front vs back (for explanation) ===
     const frontVisibleDeduction = (
