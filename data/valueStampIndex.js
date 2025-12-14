d35bd87ac8aaafd828edfc07456af7885ce3e9d2
SintoGraves <sintograves@gmail.com>
2025-12-14 10:49:12 -0500

    Update valueStampIndex.js

--- data/valueStampIndex.js
+++ data/valueStampIndex.js
@@ -524,8 +524,9 @@
     "worlds unknown#8": true
   };
 
-  // Derived convenience list used by title suggestion logic (already normalized)
+  // Derived convenience list used by title suggestions
   CGT.KNOWN_TITLES = Array.from(
     new Set(Object.keys(CGT.VALUE_STAMP_INDEX).map(k => k.split("#")[0]))
   );
+
 })();
