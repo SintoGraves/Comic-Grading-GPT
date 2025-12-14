704361025e71dc0c199a08cf9d030c98d93c8ae8
SintoGraves <sintograves@gmail.com>
2025-12-14 12:35:09 -0500

    Update scoreSpine.js

--- docs/grading/scoreSpine.js
+++ docs/grading/scoreSpine.js
@@ -3,7 +3,7 @@
  * Spine scoring (placeholder)
  * Namespace: window.CGT (aliased locally as CGT)
  *-------------------------------------------------*/
-const CGT = (window.CGT = window.CGT || {});
+var CGT = (window.CGT = window.CGT || {});
 
 CGT.computeSpineScore = function computeSpineScore(_form) {
   return {
