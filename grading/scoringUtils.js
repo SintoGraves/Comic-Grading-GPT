f595df312d811d9c46a9291c467e78b10645f168
SintoGraves <sintograves@gmail.com>
2025-12-14 10:08:02 -0500

    Update scoringUtils.js

--- grading/scoringUtils.js
+++ grading/scoringUtils.js
@@ -6,7 +6,7 @@
 (function () {
   "use strict";
 
-  const CGT = (window.CGT = window.CGT || {});
+  window.CGT = window.CGT || {};
 
   /*-------------------------------------------------
    * Grade Scale
