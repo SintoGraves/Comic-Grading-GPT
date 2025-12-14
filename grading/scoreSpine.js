0fc1bd6fe626b85c7a388a84db2ba4be0eded6bc
SintoGraves <sintograves@gmail.com>
2025-12-14 10:16:55 -0500

    Update scoreSpine.js

--- grading/scoreSpine.js
+++ grading/scoreSpine.js
@@ -3,19 +3,16 @@
  * Spine scoring stub (placeholder)
  * Global namespace: window.CGT
  *-------------------------------------------------*/
-(function () {
-  "use strict";
+/* grading/scoreSpine.js */
+window.CGT = window.CGT || {};
 
-  const CGT = (window.CGT = window.CGT || {});
-
-  CGT.computeSpineScore = function computeSpineScore(_form) {
-    return {
-      finalScore: 10.0,
-      baseScore: 10.0,
-      penaltyTotal: 0.0,
-      grade: CGT.pickGrade(CGT.GRADES, 10.0),
-      elements: [],
-      placeholder: true
-    };
+CGT.computeSpineScore = function computeSpineScore(form) {
+  return {
+    finalScore: 10.0,
+    baseScore: 10.0,
+    penaltyTotal: 0.0,
+    grade: CGT.pickGrade(CGT.GRADES, 10.0),
+    elements: [],
+    placeholder: true
   };
-})();
+};
